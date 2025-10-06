2989fe5f140825dc05abed1973333d8a643045fa
Jakendary <176844671+Jakendary@users.noreply.github.com>
2025-10-06 20:19:32 +0800

    feat: initial implementation of Launch class with required properties from API, begin implementation of GetNextLaunchAsync method in the APIService

--- LaunchObserver/Models/Launch.cs
+++ LaunchObserver/Models/Launch.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace LaunchObserver.Models
 {
     class Launch
     {
+        [JsonProperty("id")]
+        public string? Id { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("net")]
+        public DateTime? LaunchDate { get; set; }
+
+        [JsonProperty("launch_service_provider.name")]
+        public string? LaunchProvider { get; set; }
+
+        [JsonProperty("mission.name")]
+        public string? MissionName { get; set; }
+
+        [JsonProperty("mission_description")]
+        public string? MissionDescription { get; set; }
+
+        [JsonProperty("pad.name")]
+        public string? PadName { get; set; }
+
+        [JsonProperty("rocket.configuration.name")]
+        public string? RocketName { get; set; }
+
+        [JsonProperty("image.image_url")]
+        public string? Image { get; set; }
     }
 }
--- LaunchObserver/Services/APIService.cs
+++ LaunchObserver/Services/APIService.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LaunchObserver.Models;
+using System.Net;
 
 namespace LaunchObserver.Services
 {
     class APIService
     {
+        const string base_url = "https://lldev.thespacedevs.com/2.3.0/";
+
+        private async Task<Launch> GetNextLaunchAsync()
+        {
+            string url = $"{base_url}/launches/?limit=1&...";
+
+            HttpClient client = new();
+
+            HttpRequestMessage request = new(HttpMethod.Get, url);
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                //something went wrong
+            }
+
+
+        }
     }
 }
